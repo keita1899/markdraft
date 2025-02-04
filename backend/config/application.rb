a7fb3c8dd81fe6ab9ec63965dfbff48909f9cd2b
keita1899 <saboten18k@gmail.com>
2025-02-04 16:35:00 +0900

    chore: rspecを使ったテストのセットアップ

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,5 +23,14 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.helper false
+      g.test_framework :rspec,
+                       system_specs: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       request_specs: true
+    end
   end
 end
