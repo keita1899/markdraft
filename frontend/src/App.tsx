37a21456f5a19b964614717dd36689d68f853fa0
keita1899 <saboten18k@gmail.com>
2025-02-04 21:23:50 +0900

    refactor: APIのURLを環境変数に修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,11 @@ import Router from './routes/Router'
 import { fetcher } from './utils'
 
 const App = () => {
-  const url = 'http://localhost:3000/hello'
+  const url = `${process.env.REACT_APP_API_URL}/hello`
   const { data, error } = useSWR(url, fetcher)
 
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
-
-  console.log(data)
 
   return (
     <>
