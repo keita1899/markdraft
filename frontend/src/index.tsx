11205e767a21ebfb1e2007514b36176571ae9374
keita1899 <saboten18k@gmail.com>
2025-02-04 20:58:43 +0900

    chore: MaterialUIをセットアップ

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,20 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import theme from './styles/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@mui/material'
+
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
     </div>
   )
 }
