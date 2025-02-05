9d9fb48568b8b4319eb8e0cda0d1582f77eb9a96
keita1899 <saboten18k@gmail.com>
2025-02-05 20:22:55 +0900

    feat: エラーページコンポーネントを作成

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import InternalServerError from '../pages/InternalServerError'
 import NotFound from '../pages/NotFound'
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/500" element={<InternalServerError />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
