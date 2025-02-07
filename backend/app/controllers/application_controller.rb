bf06d03601690d3728ae513a87e7255578d2c213
keita1899 <saboten18k@gmail.com>
2025-02-07 23:51:15 +0900

    refactor: JWTトークンのエンコードを専用の秘密鍵を使用するようにリファクタリング

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::API
   def encode_token(payload)
-    JWT.encode(payload, Rails.application.credentials.secret_key_base)
+    secret_key = Rails.application.credentials.jwt[:secret_key]
+
+    raise "Missing secret_key for JWT in credentials" if secret_key.blank?
+
+    JWT.encode(
+      payload.merge(
+        exp: 24.hours.from_now.to_i,
+        iat: Time.current.to_i,
+        iss: "markdraft",
+      ),
+      secret_key,
+      "HS256",
+    )
   end
 end
