26a8f2962ad15e43e7ee33e35f0a2c186d2ee6f7
keita1899 <saboten18k@gmail.com>
2025-02-14 16:02:19 +0900

    fix: contentのデータ型を変更するマイグレーションを実行

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_11_034416) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_14_065914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "drafts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
-    t.string "content", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_drafts_on_user_id"
