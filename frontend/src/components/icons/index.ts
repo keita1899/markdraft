500cb9e3926d393663bebefdc6814d8add340c31
keita1899 <saboten18k@gmail.com>
2025-02-14 15:02:21 +0900

    feat: アイコンを1つのファイルにまとめる

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,5 @@
+import EditIcon from '@mui/icons-material/Edit'
+import SplitscreenIcon from '@mui/icons-material/Splitscreen'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+
+export { EditIcon, VisibilityIcon, SplitscreenIcon }
